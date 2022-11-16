3a0e4218c1fc5d1ee5b61fadce40dee18f814a96
Jan Kopecký <jan.kope@seznam.cz>
2022-11-17 00:12:44 +0100

    pop-up login fix

--- index.php
+++ index.php
@@ -11,10 +11,12 @@ ob_start(); ?>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
               integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
               crossorigin="anonymous">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+                crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> <!-- navbar -->
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+        <!-- popup login -->
         <title>Document</title>
     </head>
     <body>
@@ -72,7 +74,7 @@ ob_start(); ?>
 
         <div class="overlay" id="introduction"><h1>FOTOGALERIE</h1>
             <h2>MÍŠA HALÍKOVÁ</h2></div>
-        <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+        <div id="myCarousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active"
                         aria-label="Slide 1"></button>
@@ -110,7 +112,10 @@ ob_start(); ?>
                         </svg>
                     </div>
                     <h4 class="modal-title">Login</h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="ico-times"
+                                                                                                   role="img"
+                                                                                                   aria-label="Cancel"></i>
+                    </button>
                 </div>
                 <div class="modal-body">
                     <form method="post">
@@ -147,10 +152,6 @@ ob_start(); ?>
     </footer>
 
     <script src="photo-gallery/javascript.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
-            crossorigin="anonymous"></script>
-
 
     </body>
 
