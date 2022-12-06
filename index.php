002edb0e96d6fb6fb2e4a3aeed02539e8ddb8d34
Jan Kopecký <jan.kope@seznam.cz>
2022-12-06 17:44:11 +0100

    navbar icons update

--- index.php
+++ index.php
@@ -57,17 +57,11 @@ ob_start(); ?>
             <div class="modal-content">
                 <div class="modal-header">
                     <div class="avatar">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                            <g>
-                                <path fill="none" d="M0 0h24v24H0z"/>
-                                <path d="M20 22H4v-2a5 5 0 0 1 5-5h6a5 5 0 0 1 5 5v2zm-8-9a6 6 0 1 1 0-12 6 6 0 0 1 0 12z"/>
-                            </g>
-                        </svg>
+                        <img src="photo-gallery/icons/user-icon.svg" alt="user-black-icon"/>
                     </div>
                     <h4 class="modal-title">Login</h4>
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="ico-times"
-                                                                                                   role="img"
-                                                                                                   aria-label="Cancel"></i>
+                                                                                                   role="img" aria-label="Cancel"></i>
                     </button>
                 </div>
                 <div class="modal-body">
--- photo-gallery/nav.php
+++ photo-gallery/nav.php
@@ -9,11 +9,14 @@
                 <li><a class="link" href="<?php echo ($_SERVER['REQUEST_URI'] == "/index.php") ? "photo-gallery/gallery.php" : "gallery.php" ?>">Gallery</a></li>
                 <li><a class="link" href="<?php echo ($_SERVER['REQUEST_URI'] == "/index.php") ? "photo-gallery/about.php" : "about.php" ?>">About</a></li>
                 <div class="social-icons-nav">
-                    <li class="test"><a href="#">
+                    <li><a href="#">
                             <img src="<?php echo ($_SERVER['REQUEST_URI'] == "/index.php") ? "photo-gallery/icons/instagram-white.svg" : "icons/instagram-white.svg" ?>" alt="instagram-white-logo"/>
                         </a></li>
                     <li><a href="#">
                             <img src="<?php echo ($_SERVER['REQUEST_URI'] == "/index.php") ? "photo-gallery/icons/facebook-white.svg" : "icons/facebook-white.svg" ?>" alt="facebook-white-logo"/>
+                        </a></li>
+                    <li><a href="#myModal" data-toggle="modal">
+                            <img class="login-icon" src="<?php echo ($_SERVER['REQUEST_URI'] == "/index.php") ? "photo-gallery/icons/login-icon.svg" : "icons/login-icon.svg" ?>" alt="login-white-logo"/>
                         </a></li>
                 </div>
 
