900fae38121a3c49bcc0e64a7ccfdcf21d015002
Jan Kopecký <jan.kope@seznam.cz>
2023-01-18 20:49:04 +0100

    add upload image to database

--- photo-gallery/administration.php
+++ photo-gallery/administration.php
@@ -1,1 +1,7 @@
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <label>Select Image File:</label>
+    <input type="file" name="image">
+    <input type="submit" name="submit" value="Upload">
+</form>
+
 
---
+++ photo-gallery/upload.php
@@ -0,0 +1,57 @@
+<?php
+// Include the database configuration file
+require_once 'dbConfig.php';
+
+function compress($source, $destination, $quality)
+{
+
+    $info = getimagesize($source);
+
+    if ($info['mime'] == 'image/jpeg')
+        $image = imagecreatefromjpeg($source);
+
+    elseif ($info['mime'] == 'image/gif')
+        $image = imagecreatefromgif($source);
+
+    elseif ($info['mime'] == 'image/png')
+        $image = imagecreatefrompng($source);
+
+    imagejpeg($image, $destination, $quality);
+
+    return $destination;
+}
+
+// If file upload form is submitted
+$status = $statusMsg = '';
+if(isset($_POST["submit"])){
+    $status = 'error';
+    if(!empty($_FILES["image"]["name"])) {
+        // Get file info
+        $fileName = basename($_FILES["image"]["name"]);
+        $fileType = pathinfo($fileName, PATHINFO_EXTENSION);
+
+        // Allow certain file formats
+        $allowTypes = array('jpg','png','jpeg','gif');
+        if(in_array($fileType, $allowTypes)){
+            $image = $_FILES['image']['tmp_name'];
+            $imgContent = addslashes(file_get_contents($image));
+
+            // Insert image content into database
+            $insert = $conn->query("INSERT into images (image, created) VALUES ('$imgContent', NOW())");
+
+            if($insert){
+                $status = 'success';
+                $statusMsg = "File uploaded successfully.";
+            }else{
+                $statusMsg = "File upload failed, please try again.";
+            }
+        }else{
+            $statusMsg = 'Sorry, only JPG, JPEG, PNG, & GIF files are allowed to upload.';
+        }
+    }else{
+        $statusMsg = 'Please select an image file to upload.';
+    }
+}
+
+// Display status message
+echo $statusMsg;
