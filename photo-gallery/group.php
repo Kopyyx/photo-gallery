65cdf0e40d672af23a06fb16a1fc1b57305fa642
Jan Kopecký <jan.kope@seznam.cz>
2023-02-26 20:45:53 +0100

    add displaying images from db

--- photo-gallery/group.php
+++ photo-gallery/group.php
@@ -1,32 +1,34 @@
-<?php require_once('components/head.php');
+<?php
+require_once('components/head.php');
 require_once('components/nav.php');
 
-$button = " <button type='button' class='btn btn-primary admin_add_button' data-bs-toggle='modal' data-bs-target='#image_upload'>+</button>";
+require_once "dbConfig.php";
+
+$sql = "SELECT * FROM group_photos ORDER BY id ASC";
+$result = mysqli_query($conn, $sql);
+
+$button = "<button type='button' class='btn btn-primary admin_add_button' data-bs-toggle='modal' data-bs-target='#image_upload'>+</button>";
 ?>
-<section class="opening-bg-group" >
-    <h4 class="gallery_title text-center">SKUPINOVÉ FOTOGRAFIE</h4>
+
+<section class="opening-bg-group">
+    <h4 class="gallery_title text-center">Skupinové fotografie</h4>
 </section>
 
 <section class="gallery">
     <div class="gallery_container">
         <ul class="image-gallery d-flex align-items-center flex-wrap">
-            <li>
-                <img src="photos/group4.jpg" alt="hockey-photo" loading="lazy"/>
-            </li>
-            <li>
-                <img src="photos/group1.jpg" alt="hockey-photo" loading="lazy"/>
-            </li>
-            <li>
-                <img src="photos/group4.jpg" alt="hockey-photo" loading="lazy"/>
-            </li>
-            <li>
-                <img src="photos/group1.jpg" alt="hockey-photo" loading="lazy"/>
-            </li>
-            <?php if ($_SESSION["loggedIn"]) echo $button ?>
+            <?php
+            // Cyklus pro zobrazení jednotlivých fotografií
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<li><img src='photos/" . $row['name'] . "' alt='" . $row['name'] . "' loading='lazy'/></li>";
+                }
+            }
+            if ($_SESSION["loggedIn"]) echo $button;
+            ?>
         </ul>
     </div>
-
 </section>
-
-<?php require_once('components/footer.php');  ?>
-
+<?php
+require_once('components/footer.php');
+?>
--- photo-gallery/upload.php
+++ photo-gallery/upload.php
@@ -1,38 +1,6 @@
 <?php
 // Include the database configuration file
 require_once 'dbConfig.php';
-function compressImage($source, $destination, $quality) {
-    // Get image info
-    $imgInfo = getimagesize($source);
-    $mime = $imgInfo['mime'];
-
-    // Create a new image from file
-    switch($mime){
-        case 'image/jpeg':
-            $image = imagecreatefromjpeg($source);
-            break;
-        case 'image/png':
-            $image = imagecreatefrompng($source);
-            break;
-        case 'image/gif':
-            $image = imagecreatefromgif($source);
-            break;
-        default:
-            $image = imagecreatefromjpeg($source);
-    }
-
-    // Save image
-    imagejpeg($image, $destination, $quality);
-
-    // Return compressed image
-    return $destination;
-}
-function convert_filesize($bytes, $decimals = 2) {
-    $size = array('B','KB','MB','GB','TB','PB','EB','ZB','YB');
-    $factor = floor((strlen($bytes) - 1) / 3);
-    return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$size[$factor];
-}
-$uploadPath = "uploads/";
 
 $statusMsg = '';
 $status = 'danger';
@@ -45,46 +13,29 @@ if(isset($_POST["submit"])) {
     if (!empty($_FILES["image"]["name"])) {
         // File info
         $fileName = basename($_FILES["image"]["name"]);
-        $imageUploadPath = $uploadPath . $fileName;
-        $fileType = pathinfo($imageUploadPath, PATHINFO_EXTENSION);
 
-        // Allow certain file formats
-        $allowTypes = array('jpg', 'png', 'jpeg', 'gif');
-        if (in_array($fileType, $allowTypes)) {
-            // Image temp source and size
-            $imageTemp = $_FILES["image"]["tmp_name"];
-            $imageSize = convert_filesize($_FILES["image"]["size"]);
-
-            // Compress size and upload image
-            $compressedImage = compressImage($imageTemp, $imageUploadPath, 75);
-
-
-            if ($compressedImage) {
-                $compressedImageSize = filesize($compressedImage);
-                $compressedImageSize = convert_filesize($compressedImageSize);
-
-                if (str_contains($url, "hockey")){
-                    $tb = "hockey_photos";
-                }
-                elseif(str_contains($url, "couple")){
-                    $tb = "couple_photos";
-                }
-                elseif(str_contains($url, "group")){
-                    $tb = "group_photos";
-                }
-                $insert = $conn->query("INSERT INTO ".$tb." (id, image) VALUES ('".$compressedImage."', NOW())");
-                $statusMsg = "Komprese a nahrání proběhlo úspěšně.";
-                echo "<script> window.location.replace('$url');alert('$statusMsg')</script>";
-            } else {
-                $statusMsg = "Chyba :((";
-                echo "<script> window.location.replace('$url');alert('$statusMsg')</script>";
-            }
-        } else {
-            $statusMsg = 'Sorry, only JPG, JPEG, PNG, & GIF files are allowed to upload.';
-            echo "<script> window.location.replace('$url');alert('$statusMsg')</script>";
+        // Insert image information into database
+        if (str_contains($url, "hockey")) {
+            $tb = "hockey_photos";
+        } elseif (str_contains($url, "couple")) {
+            $tb = "couple_photos";
+        } elseif (str_contains($url, "group")) {
+            $tb = "group_photos";
         }
-    } else {
-        $statusMsg = 'Please select an image file to upload.';
-        echo "<script> window.location.replace('$url');alert('$statusMsg')</script>";
+        $stmt = $conn->prepare("INSERT INTO $tb (name) VALUES (?)");
+        $stmt->bind_param("s", $fileName);
+        if ($stmt->execute()) {
+            $statusMsg = "Nahrání proběhlo úspěšně.";
+            $status = "success";
+        } else {
+            $statusMsg = "Chyba při nahrávání do databáze :((";
+        }
+
+// Display alert with status message
+        echo "<script>alert('$statusMsg');</script>";
+
+// Redirect back to previous page
+        echo "<script>window.location.replace('$url');</script>";
+        exit();
     }
 }
